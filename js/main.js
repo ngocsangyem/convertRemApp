f0d3f39bdb6ac308632d2447d52d63c068cae454
Nguyen Sang <nnsang24@gmail.com>
2018-07-15 21:25:41 +0700

    add source

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+document.getElementById('numberInput').addEventListener('input',function(e){
+	// get input default number id
+
+	const myInputNumber = document.getElementById('numberDefaultInput')
+
+	// get default number value
+	const defaultNumber = myInputNumber.value;
+	// get rem number value
+	const rems = e.target.value;
+
+	// if defaul null
+
+	// if (myInputNumber == null || myInputNumber == "") {
+	// 	myInputNumber.value = "14"
+	// }
+
+	// rem / default value
+	document.getElementById('remOutput').innerHTML = Math.round((rems/defaultNumber)*100)/100;
+
+
+})
